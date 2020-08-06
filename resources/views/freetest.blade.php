ad6046b2310eae89d0c06f0529117abe2055d825
Marina00Matta <marinamedhat_19@hotmail.com>
2020-08-06 17:22:15 +0200

    Sign Up form and database connection

--- resources/views/freetest.blade.php
+++ resources/views/freetest.blade.php
@@ -21,22 +21,60 @@
 
 		<!-- Heading -->
 			<div id="heading" >
-				<h1>Generic Page</h1>
+				<h1>Free Test Service</h1>
 			</div>
+
+			
 
 		<!-- Main -->
 			<section id="main" class="wrapper">
 				<div class="inner">
 					<div class="content">
 						<header>
-							<h2>Feugiat consequat</h2>
+							<h2>Sign Up For Free</h2>
 						</header>
-						<p>Lorem ipsum dolor sit accumsan interdum nisi, quis tincidunt felis sagittis eget. tempus euismod. Magna et cursus lorem faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt felis sagittis eget. tempus euismod tempus. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt felis sagittis eget. tempus euismod. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac sed amet praesent. Nunc lacinia ante nunc ac gravida lorem ipsum dolor sit amet dolor feugiat consequat. </p>
-						<p>Lorem ipsum dolor sit accumsan interdum nisi, quis tincidunt felis sagittis eget. tempus euismod. Magna et cursus lorem faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt felis sagittis eget. tempus euismod tempus. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt felis sagittis eget. tempus euismod. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac sed amet praesent. Nunc lacinia ante nunc ac gravida lorem ipsum dolor sit amet dolor feugiat consequat. </p>
-						<hr />
-						<h3>Magna odio tempus commodo</h3>
-						<p>In arcu accumsan arcu adipiscing accumsan orci ac. Felis id enim aliquet. Accumsan ac integer lobortis commodo ornare aliquet accumsan erat tempus amet porttitor. Ante commodo blandit adipiscing integer semper orci eget. Faucibus commodo adipiscing mi eu nullam accumsan morbi arcu ornare odio mi adipiscing nascetur lacus ac interdum morbi accumsan vis mi accumsan ac praesent.</p>
-						<p>Felis sagittis eget tempus primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt felis sagittis eget. tempus euismod. Magna sed etiam ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus lorem ipsum dolor sit amet nullam.</p>
+			
+									<form method="post" action="#">
+										<div class="row gtr-uniform">
+											<div class="col-6 col-12-xsmall">
+											<h3> Name</h3>
+												<input type="text" name="name" id="name" value="" placeholder="Name" />
+											</div>
+											<div class="col-6 col-12-xsmall">
+											<h3> Email</h3>
+												<input type="email" name="email" id="email" value="" placeholder="Email" />
+											</div>
+
+											
+											<div class="col-6 col-12-xsmall">
+												<h3> MAC Address</h3>
+												<input type="text" name="name" id="name" value="" placeholder="MAC Address" />
+											</div>
+											<div class="col-6 col-12-xsmall">
+												<h3> IP Address</h3>
+												<input type="email" name="email" id="email" value="" placeholder="IP Address" />
+											</div>
+											
+
+											<!-- Break -->
+											<div class="col-12">
+											<h3>choose Your Package</h3>
+												<select name="category" id="category">
+													<option value="">- Select -</option>
+													<option value="alpha">Package one</option>
+													<option value="beta">Package two</option>
+													<option value="gamma">Package three</option>
+													
+												</select>
+</div>
+											<div class="col-12">
+												<ul class="actions">
+													<li><input type="submit" value="Submit Form" class="primary" /></li>
+													<li><input type="reset" value="Reset" /></li>
+												</ul>
+											</div>
+										</div>
+									</form>
 					</div>
 				</div>
 			</section>
